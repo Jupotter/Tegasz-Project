88047e4757389eaf1e03b314fa0edece129416a9
Julien Garagnon <Jupotter.g@gmail.com>
2012-01-25 23:02:46 +0100

    Premiers fichiers de test makefile

---
+++ lecteur/main.c
@@ -0,0 +1,16 @@
+﻿/* 
+ * main.c: contains the program entry point.
+ */ 
+
+/* Standard headers */
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Hello World!\n");
+  return (0);
+}
+
+/* End main.c */
